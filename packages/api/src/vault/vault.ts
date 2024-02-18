4782c3cd5f4f379d49486087b608597cc9a53d76
Nick Palenchar <nickpal@nickpalenchar.com>
2024-02-18 13:59:00 -0500

    vault

---
+++ packages/api/src/vault/vault.ts
@@ -0,0 +1,49 @@
+import { PrismaClient } from '@prisma/client';
+import { config } from '../config';
+import { add } from 'date-fns';
+import crypto from 'node:crypto';
+
+const prisma = new PrismaClient();
+
+interface Vault {
+  get: (id: string) => Promise<string | null>,
+  save: (value: string, { id, exp }: { id?: string, exp?: Date} ) => Promise<string>,
+}
+
+const encrypt = async (input: string) => {
+  const iv = crypto.randomBytes(16);
+  const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(config.SecretsKey), iv);
+  const encrypted = cipher.update(input, 'utf-8', 'hex') + cipher.final('hex');
+  return { value: encrypted, iv }
+}
+
+const decrypt = async(input: string, iv: Buffer) => {
+  const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(config.SecretsKey), iv);
+  const decrypted = decipher.update(input, 'hex', 'utf-8') + decipher.final('utf-8');
+  return decrypted;
+}
+
+export const vault: Vault = {
+  async get(key) {
+    const secret = await prisma.secrets.findFirst({
+      where: { id: key}
+    });
+    if (!secret) {
+      return null;
+    }
+    return decrypt(secret.encryptedValue, secret.iv);
+  },
+  /** Returns id which can also be used for referencing */
+  async save(value, {id, exp = add(new Date, { days: 30 }) }) {
+    const { value: encrypted, iv } = await encrypt(value);
+    const secret = await prisma.secrets.create({
+      data: {
+        encryptedValue: encrypted,
+        iv,
+        ...(id && { id }),
+        exp
+      }
+    })
+    return secret.id;
+  }
+}
